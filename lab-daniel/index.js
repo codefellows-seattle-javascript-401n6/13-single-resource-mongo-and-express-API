fc79a03992e73bc6106ec8d053c6842b96278f54
daniel <dpillay03@gmail.com>
2018-03-16 15:22:28 -0700

    server is running

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -2,4 +2,10 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+app.listen(PORT, () => {
+    console.log(`localhost://${PORT}`);
+});
