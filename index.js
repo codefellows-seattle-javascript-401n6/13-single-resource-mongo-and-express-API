fcacda50e3272019e826b4bcb2e87975e8735b9c
Caitlin <caitlinsweeneylmt@gmail.com>
2018-03-17 12:07:28 -0700

    working schemas!! methods next

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.connect('');
+mongoose.connect('mongodb://localhost:27017');
 
 
 let dollSchema = new mongoose.Schema({
   name: String,
-  order: number,
+  order: Number,
   
 });
 
 let matryoshkaSchema = new mongoose.Schema({
   dolls : [dollSchema],
-  layers: number,
+  layers: Number,
 });
 
-matryoshkaSchema.methods.layers = function() {
-  let layers = 0;
-  this.dolls.forEach(() => {
-    layers = layers ++;
-  });
-  return layers;
-};
+// matryoshkaSchema.methods.layers = function() {
+//   let layers = 0;
+//   this.dolls.forEach(() => {
+//     layers = layers ++;
+//   });
+//   return layers;
+// };
+
+// matryoshkaSchema.methods.placement = function(){
+//   let placement = [dolls.length];
+  
+//   this.dolls.forEach(() =>{
+//     if (placement.length <= 0){
+//       this.order 
+
+//     }
+//   });
+// };
 
 let Doll = mongoose.model('Doll', dollSchema);
 let Matryoshka = mongoose.model('Matryoshka', matryoshkaSchema);
 
-let moira = new Doll({name:'Moira', order})
+let moira = new Doll({name:'Moira', order: 1});
+let yulia = new Doll({name:'Yulia', order: 2});
+
+
+Promise.all([
+  moira.save(),
+  yulia.save()
+])
+  .then(dolls => {
+    let matryoshka  = new Matryoshka({dolls: dolls});
+    return matryoshka.save();
+  })
+  .then(savedMatryoshka => {
+    console.log('Matryoshka:', savedMatryoshka);
+  })
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log('Error', err);
+    mongoose.disconnect();
+  });
