812f8408b5ee984c969569b3c5757f5af68e3e0e
daniel <dpillay03@gmail.com>
2018-03-18 23:14:23 -0700

    second test passing. returning 404 for invalid id

--- lab-daniel/test/car.test.js
+++ lab-daniel/test/car.test.js
@@ -4,12 +4,19 @@ const server = 'http://localhost:3000';
 const agent = require('superagent');
 
 
-describe('GET REQUEST', () => {
+describe('Testing RESTful API', () => {
     it('Should return 404 if the route is not found', (resolve, reject) =>{
         agent.get(`${server}/`)
         .end((err, res) => {
             expect(res.status).toBe(404);
             resolve(); 
+        })
+    })
+    it('Should return 404 for invalid id', (resolve, reject) => {
+        agent.get(`${server}/api/bicycle`)
+        .end((err, res) => {
+            expect(res.status).toBe(404);
+            resolve();
         })
     })
 })
