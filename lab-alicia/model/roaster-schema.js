11f0c969041a7a3417145689cede859a5369051e
alicialycan <alicialycan@gmail.com>
2018-06-19 11:43:20 -0700

    update packages and tests

--- lab-alicia/model/roaster-schema.js
+++ lab-alicia/model/roaster-schema.js
@@ -1,19 +1,15 @@
+'use strict';
+
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/coffees');
 
 const roasterSchema = new Schema({
-    coffeeBlends: [{
-        type: Schema.Types.ObjectId,
-        ref: 'coffee'
-    }]
+  coffeeBlends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'coffee'
+  }]
 });
-
-roasterSchema.methods.total = () => {
-    let total = 0;
-    this.coffeeBlends.forEach(coffee => {
-        total += coffee.roast;
-    });
-    return total;
-};
 
 module.exports = mongoose.model('roaster', roasterSchema);
--- lab-alicia/server.js
+++ lab-alicia/server.js
@@ -15,5 +15,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
+  console.log(`listening on ${PORT}`);
 });
